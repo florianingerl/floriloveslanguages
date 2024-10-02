f7c1f1052f50533fce20bad9a8879d4c84fb714f
Florian Ingerl <imelflorianingerl@gmail.com>
2024-10-02 20:53:05 +0200

    VueHideShow also works!

--- floriloveslanguages/src/views/HomeView.vue
+++ floriloveslanguages/src/views/HomeView.vue
@@ -5,11 +5,12 @@ import Stupid from "./../components/Test.vue";
 import ProvideArrayTest from "./../components/ProvideArrayTest.vue";
 import VueWholeSentence from "./../components/vue-whole-sentence.vue";
 import VueMCGaps from "./../components/vue-mc-gaps.vue";
+import VueHideShow from "./../components/vue-hideshow.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent(
   {
-    components: { TheWelcome, Stupid, VueWordOrder, ProvideArrayTest, VueWholeSentence, VueMCGaps }
+    components: { TheWelcome, Stupid, VueWordOrder, ProvideArrayTest, VueWholeSentence, VueMCGaps, VueHideShow }
   }
 );
 </script>
@@ -26,5 +27,14 @@ export default defineComponent(
 
     <h1>VueMCGaps</h1>
     <VueMCGaps lg="en" gaptext="Nenad is {smart} and Florian is {smart} too."></VueMCGaps>
+
+    <h1>VueHideShow</h1>
+    <VueHideShow lg="en">
+    <p>Florian is smart and so is Nenad.</p>
+    <p>Florian is smart and so is Nenad.</p>
+    <p>Florian is smart and so is Nenad.</p>
+    <p>Florian is smart and so is Nenad.</p>
+    <p>Florian is smart and so is Nenad.</p>
+    </VueHideShow>
   </main>
 </template>
