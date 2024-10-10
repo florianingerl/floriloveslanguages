73f7826fd6cf2833d4f1a645330df9b1ffabc72a
Florian Ingerl <imelflorianingerl@gmail.com>
2024-10-10 19:49:02 +0200

    Updates the select component!

--- src/components/atoms/vue-mc-gaps.vue
+++ src/components/atoms/vue-mc-gaps.vue
@@ -26,21 +26,77 @@
     <option v-for="op in gap.options">{{ op }}</option>
   </select> -->
 
-  <Select :v-bind="gap.guess" :disabled="validated" v-if="gap.options" :class="{ notcorrect: validated && gap.guess != gap.solution, correct: validated && gap.guess === gap.solution }">
-          <FormControl>
-            <SelectTrigger>
-              <SelectValue placeholder="Select something" />
-            </SelectTrigger>
-          </FormControl>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem v-for="op in gap.options" :value="op">
-                {{ op }}
-              </SelectItem>
-              
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+  <SelectRoot v-if="gap.options" :disabled="validated" :class="{ notcorrect: validated && gap.guess != gap.solution, correct: validated && gap.guess === gap.solution }" v-model="gap.guess">
+    <SelectTrigger
+      class="inline-flex min-w-[160px] items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-grass11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-green9 outline-none"
+      aria-label="Customise options"
+    >
+      <SelectValue placeholder="Select the right thing..." />
+      <Icon
+        icon="radix-icons:chevron-down"
+        class="h-3.5 w-3.5"
+      />
+    </SelectTrigger>
+
+    <SelectPortal>
+      <SelectContent
+        class="min-w-[160px] bg-white rounded shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade z-[100]"
+        :side-offset="5"
+      >
+        <SelectScrollUpButton class="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+          <Icon icon="radix-icons:chevron-up" />
+        </SelectScrollUpButton>
+
+        <SelectViewport class="p-[5px]">
+         <!-- <SelectLabel class="px-[25px] text-xs leading-[25px] text-mauve11">
+            Fruits
+          </SelectLabel> -->
+          <SelectGroup>
+            <SelectItem
+              v-for="(option, index) in gap.options"
+              :key="index"
+              class="text-[13px] leading-none text-grass11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-green9 data-[highlighted]:text-green1"
+              :value="option"
+            >
+              <SelectItemIndicator class="absolute left-0 w-[25px] inline-flex items-center justify-center">
+                <Icon icon="radix-icons:check" />
+              </SelectItemIndicator>
+              <SelectItemText>
+                {{ option }}
+              </SelectItemText>
+            </SelectItem>
+          </SelectGroup>
+
+          <!--
+          <SelectSeparator class="h-[1px] bg-green6 m-[5px]" />
+          <SelectLabel class="px-[25px] text-xs leading-[25px] text-mauve11">
+            Vegetables
+          </SelectLabel>
+          <SelectGroup>
+            <SelectItem
+              v-for="(option, index) in vegetables"
+              :key="index"
+              class="text-[13px] leading-none text-grass11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-green9 data-[highlighted]:text-green1"
+              :value="option"
+              :disabled="option === 'Courgette'"
+            >
+              <SelectItemIndicator class="absolute left-0 w-[25px] inline-flex items-center justify-center">
+                <Icon icon="radix-icons:check" />
+              </SelectItemIndicator>
+              <SelectItemText>
+                {{ option }}
+              </SelectItemText>
+            </SelectItem>
+          </SelectGroup> -->
+        </SelectViewport>
+
+        <SelectScrollDownButton class="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+          <Icon icon="radix-icons:chevron-down" />
+        </SelectScrollDownButton>
+      </SelectContent>
+    </SelectPortal>
+  </SelectRoot>
+
   <Input v-if="!validated && gap.gap" :style="{ width: ( gap.gap.length + 2 ) + 'ch' }" type="text" v-model="gap.guess"> </Input>
   <span v-if="validated && gap.gap"  :class="{ notcorrect: validated && gap.guess != gap.solution, correct: validated && gap.guess === gap.solution }">{{ gap.guess }} </span>
   <span v-if="validated && gap.gap && gap.solution != gap.guess" class="correct">{{ gap.solution }} </span>
@@ -81,21 +137,32 @@
 </template>
 
 <script lang="ts">
+import { Icon } from '@iconify/vue'
+import {
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectItemIndicator,
+  SelectItemText,
+  SelectLabel,
+  SelectPortal,
+  SelectRoot,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+  SelectViewport,
+} from 'radix-vue'
+
+
 import { ref, defineComponent } from "vue";
 import type { Languages } from "../../types/Languages.ts";
 import type { PropType } from "vue";
 import type { Gap } from "../../types/Gap.ts";
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
+
 
 
 export default defineComponent({
