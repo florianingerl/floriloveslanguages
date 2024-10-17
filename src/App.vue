348e2d75153cd68f2644e518ec3bcf55f32cfdd6
Florian Ingerl <imelflorianingerl@gmail.com>
2024-10-17 09:37:52 +0200

    The routing works now!

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import { Button } from '@/components/ui/button'
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
 import { RouterLink, RouterView } from 'vue-router'
-import ONavBarMenu from '@/components/organisms/ONavBarMenu.vue';
 </script>
 
 <template>
-  
-  <RouterView />
-  <!--
-  <ONavBarMenu />
-  <hr>
-  <main>
-    <RouterView />
-  </main>
-  <footer>
-    <hr>
-    Footer
-  </footer> -->
-
-
-  <div>
-   <Button>Click me</Button>
-  </div>
-
-  <div>
- <Select>
-    <SelectTrigger class="w-[180px]">
-      <SelectValue placeholder="Select a fruit" />
-    </SelectTrigger>
-    <SelectContent>
-      <SelectGroup>
-        <SelectLabel>Fruits</SelectLabel>
-        <SelectItem value="apple">
-          Apple
-        </SelectItem>
-        <SelectItem value="banana">
-          Banana
-        </SelectItem>
-        <SelectItem value="blueberry">
-          Blueberry
-        </SelectItem>
-        <SelectItem value="grapes">
-          Grapes
-        </SelectItem>
-        <SelectItem value="pineapple">
-          Pineapple
-        </SelectItem>
-      </SelectGroup>
-    </SelectContent>
-  </Select>
-  </div>
+  <RouterView /> 
 </template>
 
 
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { createApp } from 'vue';
 //import App from './App.vue'
-import HomeView from '@/views/vue-home.vue';
-//import App from './App.vue';
+//import HomeView from '@/views/vue-home.vue';
+import App from './App.vue';
 //import HomeView from './views/AboutView.vue';
 import router from './router';
 
-const app = createApp(HomeView);
+const app = createApp(App);
 app.provide("window", window);
 
 app.use(router);
