d58a67bf96878467e878fd5602c73d00841664df
Florian Ingerl <imelflorianingerl@gmail.com>
2024-11-09 07:54:35 +0100

    Resolves bug with dictionary!

--- src/components/atoms/layouts/Dictionary.vue
+++ src/components/atoms/layouts/Dictionary.vue
@@ -19,31 +19,31 @@
 
 <script setup lang="ts">
 import type { Dictionary } from "@/types/Dictionary.ts";
-import { onMounted, watch , inject } from "vue";
+import { onMounted, watch , inject, ref } from "vue";
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 
-let selectedDict = "The free dictionary French - French";
-let dictionaries : Dictionary[] = [];
+const selectedDict = ref("The free dictionary French - French");
+const dictionaries =  ref<Dictionary[]>([]);
 const window = inject("window") as any;
 
 function setDictionaries() {
          console.log(route.params.language );
    if( route.params.language === 'french' ){
-    dictionaries = [ { name: "Word Reference French - German", url: "https://www.wordreference.com/frde/" },
+    dictionaries.value = [ { name: "Word Reference French - German", url: "https://www.wordreference.com/frde/" },
                          { name: "Word Reference French - English", url: "https://www.wordreference.com/fren/" }, 
                          { name: "Larousse French - French", url: "https://www.larousse.fr/dictionnaires/francais/"},
                          { name: "The free dictionary French - French", url: "https://fr.thefreedictionary.com/" }];
    }
    else if( route.params.language === 'english' ){
-     dictionaries = [ { name: "Word Reference English - German", url: "https://www.wordreference.com/ende/" },
+     dictionaries.value = [ { name: "Word Reference English - German", url: "https://www.wordreference.com/ende/" },
                          { name: "Word Reference English - French", url: "https://www.wordreference.com/enfr/" }, 
                          { name: "Oxford Learner's dictionary English - English", url: "https://www.oxfordlearnersdictionaries.com/definition/english/"},
                          { name: "The free dictionary English - English", url: "https://en.thefreedictionary.com/" }];
    }
    else if( route.params.language === 'deutsch' ){
-     dictionaries = [ { name: "The free dictionary Deutsch - Deutsch", url: "https://de.thefreedictionary.com/" },
+     dictionaries.value = [ { name: "The free dictionary Deutsch - Deutsch", url: "https://de.thefreedictionary.com/" },
                          { name: "Word Reference Deutsch - Englisch", url: "https://www.wordreference.com/deen/" },
                          { name: "Word Reference Deutsch - Französisch", url: "https://www.wordreference.com/defr/" },
                          { name: "Word Reference Deutsch - Spanisch", url: "https://www.wordreference.com/dees/" },
@@ -51,7 +51,7 @@ function setDictionaries() {
                         ];
    }
    else if( route.params.language === 'espagnol' ){
-     dictionaries = [ { name: "Word Reference Französisch - Spanisch", url: "https://www.wordreference.com/esfr/" },
+     dictionaries.value = [ { name: "Word Reference Französisch - Spanisch", url: "https://www.wordreference.com/esfr/" },
                            { name: "The free dictionary Espagnol - Espagnol", url: "https://es.thefreedictionary.com/" },
                          { name: "Word Reference Spanisch - Deutsch", url: "https://www.wordreference.com/esde/" },
                           { name: "Word Reference Spanisch - Englisch", url: "https://www.wordreference.com/esen/" },
@@ -60,7 +60,7 @@ function setDictionaries() {
 
 
 
-   selectedDict = dictionaries[0].name;
+   selectedDict.value = dictionaries.value[0].name;
 
 }
 
@@ -68,9 +68,9 @@ function openDict(_event: Event ){
         
          let str = window.getSelection().toString() as string;
          let baseUrl = "https://fr.thefreedictionary.com/";
-         let i = dictionaries.findIndex( (dict: Dictionary ) => dict.name === selectedDict );
+         let i = dictionaries.value.findIndex( (dict: Dictionary ) => dict.name === selectedDict.value );
          if( i!= -1 ){
-            baseUrl = dictionaries[i].url;
+            baseUrl = dictionaries.value[i].url;
          }
          window.open(baseUrl + str, "_blank").focus();
        
