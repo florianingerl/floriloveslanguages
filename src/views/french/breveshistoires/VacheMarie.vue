8c95a4ad43efcbb757cbd104b26c159df4987009
Florian Ingerl <imelflorianingerl@gmail.com>
2024-12-21 22:22:01 +0100

    Adds l'histoire avec la vache!

--- src/views/french/breveshistoires/VacheMarie.vue
+++ src/views/french/breveshistoires/VacheMarie.vue
@@ -4,25 +4,85 @@
     <h1>Une vache ne peut pas donner d'huile</h1>
     <h4><i>par Florian Ingerl</i></h4>
     
-    <img style="float: right; height: 200px" src="@/assets/img/francais/poule.png"></img>
+    <img style="float: right; height: 200px" src="@/assets/img/francais/vache.jpg"></img>
     
     
     La vache avait un petit bébé dont elle prenait bien soin.
 Les deux prenaient un bain de soleil dans un vaste pré.
+<VueExercise lg="fr" :topics="['dont ou où']" instruction="Remplis les trous par dont ou où!">
+<VueMCGaps lg="fr" :gaptexts="['La vache {dont} le lait est delicieux appartient à un paysan.',
+    'Le village {où} se trouve la ferme du paysan est dans le Sud de la France.'
+]">
+
+</VueMCGaps>
+</VueExercise>
+
 Bien que la maman vache méprisait parfois les humains,
-elle ne voulait pas non plus se méprendre sur certains d'entre eux. En effet, tous n'étaient pas bons, il fallait donc se méfier mais certains d'entre eux pouvaient se révéler être de bons camarades.
+elle ne voulait pas non plus se méprendre sur certains d'entre eux. 
+En effet, tous n'étaient pas bons, il fallait donc se méfier mais certains d'entre eux pouvaient 
+se révéler être de bons camarades.
+<VueExercise lg="fr" :topics="['Mépriser, se méprendre ou se méfier']" instruction="Remplis les trous avec mépriser, méprendre ou se méfier à la bonne conjugaison!">
+    <VueMCGaps lg="fr" :gaptexts="['Quand tu as un verre plein de lait, {méfis-toi} de ne pas le renverser parce que du lait est trop précieux pour le gaspiller.',
+        'La vache {méprise} tous les taurreaux qui n\'ont pas de bonnes manières.',
+        'La vache {s\'est méprise} sur le taurreau qui lui a donné son bébé, parce que celui-lui l\'a quittée le jour après la naissance du bébé.'
+    ]">
+
+    </VueMCGaps>
+
+</VueExercise>
+
 Cependant, elle connaissait aussi des humains qui gagnaient leur pain avec des arnaques. Ces humains-là la caressaient toujours tendrement bien qu'ils  voulaient seulement obtenir d'elle autant de lait que possible.
 Et la maman vache a réalisé que ce lait-là manquait ensuite à son bébé.
-Par conséquent, elle ne pouvait s'empêcher de douter de la nature bienveillante de ces humains et elle se doutait bien qu'il y avait anguille sous roche.
-Autrefois, la vache était très extravertie. Mais à cause des ces humains malhonnêtes et avec la responsabilité de son petit bébé, elle avait changé de caractère et se méfiait de presque tout. Cela l'empêchait d'explorer de nouvelles relations sans préjudices.
-"Dans quel monde étrange vivons-nous?" s'interrogeait-elle parfois. "Revenons à l'essentiel!" se disait-elle pour retrouver un sens à sa vie.
-Elle connaissait aussi des humains sympas. Par exemple, il y avait ce petit gamin qui travaillait à la ferme et qui balayait avec grand soin sa maison et celle de son bébé, ce dont elle lui était très reconnaissante. "Comment est-ce que je pourrais exprimer ma gratitude? " se demanda la maman vache. "Lui offrir du lait? De son salaire maigre, il ne peut sûrement jamais s'acheter du lait de qualité."
+
+<VueExercise lg="fr" :topics="['manquer ou rater']" instruction="Remplis les trous par manquer ou rater a la bonne conjugaison! Quand les verbes sont possibles pour un trou, choisis toujours rater!">
+<VueMCGaps lg="fr" :gaptexts="['Un jour, un homme voulait tuer la vache avec une arme à feu, mais il l\'a {ratée.}',
+    'Dans un pré, une vache ne {manque} pas de nourriture.',
+    'Beaucoup de taurraux {manquent} de bonnes manières.',
+    'Le père {manque} à son bébé.',
+    'La vache voulait mettre son lait dans un seau, mais elle l\'a {raté} parce que il s\'est verssé à côté.'
+]">
+
+</VueMCGaps>
+
+</VueExercise>
+
+<img style="float: right; height: 200px" src="@/assets/img/francais/vache.jpg"></img>
+Par conséquent, elle ne pouvait s'empêcher de douter de la nature bienveillante 
+de ces humains et elle se doutait bien qu'il y avait anguille sous roche.
+Autrefois, la vache était très extravertie. 
+Mais à cause des ces humains malhonnêtes et avec la responsabilité de son petit bébé, 
+elle avait changé de caractère et se méfiait de presque tout. 
+<VueExercise lg="fr" :topics="['douter ou se douter']" instruction="Remplis les trous par douter ou se douter à la bonne conjugaision!">
+<VueMCGaps lg="fr" :gaptexts="['Le paysan doute de la qualité du lait parce que celui-ci n\'était pas dans le réfrigérateur pendant longtemps.',
+    'Je suis malade et je {me doute} que ce soit à cause du lait pourri que j\'ai bu hier.',
+    'Le paysan {se doute} que sa femme aie une liason, parce qu\'elle sort tous les soirs sans lui.',
+    'Le paysan {doute} de la fidèlité de sa femme bien que sa femme lui aie donné un sermon de fidèlité.'
+]">
+
+</VueMCGaps>
+</VueExercise>
+
+Cela l'empêchait d'explorer de nouvelles relations sans préjudices.
+"Dans quel monde étrange vivons-nous?" s'interrogeait-elle parfois. 
+"Revenons à l'essentiel!" se disait-elle pour retrouver un sens à sa vie.
+Elle connaissait aussi des humains sympas. 
+Par exemple, il y avait ce petit gamin qui travaillait à la ferme et qui balayait avec grand soin sa maison 
+et celle de son bébé, ce dont elle lui était très reconnaissante. 
+<VueExercise lg="fr" :topics="['se balader, bavarder ou balayer']" instruction="Remplis les trous avec se balader, bavarder ou balayer à bonne conjugaison!">
+
+</VueExercise>
+
+
+"Comment est-ce que je pourrais exprimer ma gratitude? " se demanda la maman vache. "Lui offrir du lait? De son salaire maigre, il ne peut sûrement jamais s'acheter du lait de qualité."
 Elle se dit alors qu'elle pourrait attendre le bon moment pour cela et atteignit tranquillement le bord du pré avec son bébé pour voir si le jeune garçon apparaitrait.
 Juste quand la maman vache avait rempli tout un seau avec du lait frais, le gamin surgit. On pouvait le reconnaître facilement grâce à sa casquette qu'il portait toujours, même quand il n'y avait pas de soleil.
+
+<img style="float: right; height: 200px" src="@/assets/img/francais/vache.jpg"></img>
 Elle était contente car tant qu'elle aurait du lait, elle pourrait lui en donner autant chaque jour.
 "Merci beaucoup!" s'exclama le gamin en se penchant sur le seau et en voyant le lait frais.
 "J'ai tant de travail que je ne peux pas l'accomplir sans avoir mangé quelque chose de gras." En buvant le lait, le gamin caressait la vache et pensait à haute voix: "Malheureusement, tu ne peux fournir que du lait et pas d'huile. En effet, j'aurais besoin d'huile pour ma mobylette et ma camionnette."
 "Je t'ai offert du lait, lui répondit la vache, et ceci a été fait le cœur ouvert."
+<img style="float: right; height: 200px" src="@/assets/img/francais/vache.jpg"></img>
 "Mais mon lait ne peut bien sûr pas remplacer l'huile dont tu as besoin. " Parce que la vache ne pouvait aider le gamin d'une autre manière, elle lui offrit simplement un sourire. Et en voyant ce sourire radieux, le gamin oubliait toutes ses obligations et n'avait plus besoin de conduire n'importe où.
 La morale: Un sourire peut solutionner tous vos problèmes.
 
