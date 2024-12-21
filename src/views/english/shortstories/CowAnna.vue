bfefd3479a7a6fe5d956021eb75d2e81dc4a6b2c
Florian Ingerl <imelflorianingerl@gmail.com>
2024-12-21 19:40:31 +0100

    Add new stories!

---
+++ src/views/english/shortstories/CowAnna.vue
@@ -0,0 +1,25 @@
+The cow allowed itself to be a bit lazy today.
+After having worked like  a machine for the past 4 days, it just needed a rest day.
+Despite giving a lot of milk to the humans who owned the farm where the cow, Molly lived, the humans didn't appreciate her enough. They were not too kind, neither too pretty: they had freckles all over their faces and big noses.
+And they often deafened Molly by shouting: "Hurry up to give milk!"
+Molly always put in the effort but she felt like she can never please the unkind humans so she started to think about a plan to possibly escape the farm. She had a good friend, a dog named Ben which already had a tentative of escape in the past. But he didn't succeed so he was still on the farm.
+"Why did your escape strategy fail? " Molly asked the dog. And the dog answered:
+"In fact, I was able to escape, but then I didn't know how to nourish myself and got very, very hungry and returned. As a cow, hopefully, you won't have that problem because you can drink your own milk, can't you?"
+"Yes, I think it's possible and I won't have the same problem as you did. But I don't want to go alone on this escapade, so what if we steal some sandwiches for you and then you can join me and escape together?" proposed Molly to her friend.
+"First of all, I like sandwhiches and I know where our kind owners store them. Because they have exploited me all these years like I were their slave, I would not hestitate to steal all of them. But what can I possibly eat when all the sandwhiches are gone? Can you come up with a plan?" said the dog Brian.
+"Yes, of course! I have two plans for you. Maybe you can use your teeth and hunt some small animals in the forest once we have run out of sandwiches. Or we can have a plan B and maybe find another farm for us to live where the humans treat us well and they don't have such a bad temper as our current owners." answered Molly.
+"But before we leave for good, I also want to take revenge on our kind owners!
+Let us set the chickens free. You could lift me up, so I can reach the door to their cage and we will make sure that they get far away from the farm before our owners realise their disappearance. Isn't this a concise idea? " suggested the dog.
+"I really like this idea!" agreed Molly immediately. "We should also open the gate in the garden so maybe a thief can find his way to the house and also steal some money from our owners! We can do the same, I will lift you up and you open the door there too to complete our revenge."
+"And we might also set all the pigs free!" continued Brian who was suddenly full of ideas. 
+"And the goats, the sheep and the rabbits!"
+And maybe all these animals will help to destroy the farm. So with good teamwork, we might be able to cause as much damage as possible.
+"I can't wait to bring this plan to life and hurt the owners as much as they have been hurting and exploiting us for all these years! They will deserve our revenge! When should we carry out our plans?" asked Molly.
+"We carry out this plan immediately of course! So our owners will be bankrupt by tomorrow!" 
+Letting their words be followed by actions, the cow went under the cage of the chickens and the dog climbed onto its back. The dog made an effort to reach the door of the cage.
+To their delight, their plan worked perfectly and they were able to free all the animals and even open the gate in the garden so anyone (including thieves) could walk in freely. As the last rays of sunshine disappeared on the horizon and the sun set, the peaceful farm turned into chaos.
+Molly and Brian were pretty proud of the chaos they had conjured.
+But what did the pigs do. "What do you do?" Brian shouted to them. "This is the slaughterhouse you are just going into. Go in the other direction. There is a place with a lot of dirt you can play in !"
+After the succesful plan, Molly and Brian quickly stole some sandwiches and departed on their escapade. By half past 5 in the morning, they were far away from their old farm. They felt a bit tired but very pleased with their escape.
+And they could look forward to a bright, happy future in freedom!
+The end
---
+++ src/views/english/shortstories/FoxMarie.vue
@@ -0,0 +1,54 @@
+It was a bittercold winter and the fox Brian was suffering
+heavily. What could he do to warm up?
+
+He thought he could go back to the forest and maybe he would be lucky and he would meet again the lad he saw last time. Despite of the cold, he decided to go back to the forest.
+Maybe they could become friends. Not only could this new friend chase away the feeling of coldness by playing with him, but also they could make a fire after looking for dry wood.
+Most of all, what Brian liked was non ostentatious people or environment. That was really important for him. And he would always be stubborn with that and he could even become defiant about it.
+ostentatious =
+stubborn
+defiant
+whatever
+hide
+Since the other lad had been wearing shabby clothes
+last time that he met him, he was clearly not ostentatious. "How can I find him?" thought Brian. "Where might this fox be hiding from the cold now? He might have chosen whatever place that spreads some warmth. Maybe a hole in a tree or in the earth. "
+
+Whereever he's hiding, I'm sure to find him anyway. He will probably be happy to see me bacause he knows we'll play and I'll offer him a warm place to rest.
+
+"We could also eat fresh food together because all the ice has kept my provisions fresh. I still have a lot of rabbits, chickens and deers in my fridge." thought Brian.
+whereever
+
+offer
+
+fresh food
+ice
+potion
+
+spoil
+And I still have some of this very special potion in my fridge that I really don't want to spoil. Maybe I could share it with Brian, he might enjoy it.
+pretty
+tiny
+Suddendly, he had the idea that he could attract the fox by going with his potion into the forest and spreading its marvellous scent. He was pretty sure that this idea would work. No fox could possibly resist this delicious potion.  
+But he would have to be careful that no other animal would even receive the tiniest bit of their potion.
+gap
+
+proud
+Fortunately, the gap between the forest and the valley was full of snow and then he could easily go across this gap.  He felt quite proud for thinking of using the potion to attract Brian.
+path
+to run
+While running over the snow-filled gap between valley and forest, Brian realized that
+the running warmed him up. Not only was the valley full of thick snow, but it was also reached by some rays of sunlight. 
+And who approched him there wearing these shabby clothes? "What a coincidence!" exclaimed Brian. "I was just looking for you. What do you do here." 
+"This is the path to run that I always use in the winter to warm up!" answered the other fox.
+
+
+chat
+
+amused
+
+passive
+They started to chat together for a while, and both of them were amused to listen to what happened to them the last few weeks. The day was brightened by their laugh.
+And if they had already this much fun in the winter, you can imagine how their friendship will florish in summer with sunshine and with the possiblity to have a bath in the warm water of the river crossing the forest.
+
+!The end!
+water
+sun
